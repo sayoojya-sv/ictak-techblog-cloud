a65d26a08e0b075cda3a5b7dd7446d5b02655744
sayoojya-sv <adhisayoo24@gmail.com>
2025-12-28 01:09:41 +0530

    first

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -5,8 +5,8 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,37 +14,27 @@ public class TestBase {
 
     protected WebDriver driver;
 
-    public WebDriver getDriver() {
-        return driver;
-    }
-
-    @BeforeMethod
+    @BeforeClass(alwaysRun = true)
     public void setUp() {
 
-        // Setup ChromeDriver
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-
-        // ✅ Required for Jenkins / Docker / Cloud
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.get("http://64.227.132.106/");
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     public void tearDown() {
         if (driver != null) {
             driver.quit();
-            driver = null;
         }
     }
 }
+
